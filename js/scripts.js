57ac4351d39dbaeb64959c38565ad0aefba105e3
Jason Arnold <jason@thejasonfile.com>
2015-10-20 21:25:28 -0400

    finished color functions in JS

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,20 @@ $(document).ready(function() {
 		idNum = parseInt($(this).attr('id'));
 		$(".button").css('background-color','blue');
 		$(this).css('background-color','red');
-		$(this).next().css('background-color','yellow');
-		$(this).prev().css('background-color','yellow');
 		$($(".button")[idNum-4]).css('background-color','yellow');
 		$($(".button")[idNum+4]).css('background-color','yellow');
+
+		if (idNum === 3 || idNum === 7 || idNum === 11 || idNum === 15) {
+			$(this).prev().css('background-color','yellow');
+		}
+
+		else if (idNum === 0 || idNum === 4 || idNum === 8 || idNum === 12) {
+			$(this).next().css('background-color','yellow');
+		}
+
+		else {
+			$(this).prev().css('background-color','yellow');
+			$(this).next().css('background-color','yellow');
+		}
 	})
 })
