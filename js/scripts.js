ff2253b60f13767bc03dfceac3ff2c38b15f2221
Jason Arnold <jason@thejasonfile.com>
2015-10-21 22:23:08 -0400

    changed number of buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var i, idNum;
 
-for (i=0; i<16; i++) {
+for (i=0; i<6; i++) {
 	$(".container").append("<div class='button smallbutton' id='"+i+"'></div>")
 }
 
@@ -9,21 +9,7 @@ $(document).ready(function() {
 		idNum = parseInt($(this).attr('id'));
 		$(".button").css('background-color','blue');
 		$(this).css('background-color','red');
-		$($(".button")[idNum-4]).css('background-color','yellow');
-		$($(".button")[idNum+4]).css('background-color','yellow');
-
-		if (idNum === 3 || idNum === 7 || idNum === 11 || idNum === 15) {
-			$(this).prev().css('background-color','yellow');
-		}
-
-		else if (idNum === 0 || idNum === 4 || idNum === 8 || idNum === 12) {
-			$(this).next().css('background-color','yellow');
-		}
-
-		else {
-			$(this).prev().css('background-color','yellow');
-			$(this).next().css('background-color','yellow');
-		}
-
+		$(this).next().css('background-color','yellow');
+		$(this).prev().css('background-color','yellow');
 	})
 })
